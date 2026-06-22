2225348c9b2329eada39790380637126c49c50e5
Andrei Costinescu <andrei.costinescu@yahoo.com>
2026-06-22 21:03:22 +0200

    Add very basic and initial tests for template argument parsing and validation of instantiated types
    Signed-off-by: Andrei Costinescu <andrei.costinescu@yahoo.com>

---
+++ tests/unit/test_template_argument_parsing.py
@@ -0,0 +1,223 @@
+# Copyright 2026 ConceptHierarchy Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Unit tests for parsing template arguments in a ConceptHierarchyDefinition
+including verifying template argument constraint formulae.
+"""
+
+from copy import deepcopy
+
+import pytest
+
+from concept_hierarchy.data.concept_hierarchy import ConceptHierarchy
+from concept_hierarchy.definitions.concept_definition_hidden_implementation import HiddenImplementationDefinition
+from concept_hierarchy.errors import CHSemanticError, CHSyntaxError
+from concept_hierarchy.models import ConceptHierarchyModel
+from concept_hierarchy.validator.checker import ConceptHierarchyChecker
+
+
+class TestTemplateArgumentParsing:
+    _model_data = {
+        "name": "AnimalKingdom",
+        "metadata": {"version": "1.0", "author": "Your Name"},
+        "concepts": {
+            "Concept": {"data": None},
+            "Animal": {
+                "directParents": ["Concept"],
+                "data": {
+                    "properties": {
+                        "age": "Integer",
+                        "name": "String",
+                        "type": {"valueDomain": "String"},
+                        "description": {
+                            "valueDomain": "String",
+                            "description": "Test property",
+                            "hooks": {},
+                            "confidenceHalfDecayTime": [2, "d"],
+                            "nameDefaultInstanceValuesWithThisInstanceName": True,
+                        },
+                        "_specializations": {
+                            "_forThis": {"age": {"constraint": {"ClosedInterval<Integer>": {"min": 0, "max": 14}}}}
+                        },
+                    }
+                },
+            },
+            "Dog": {
+                "directParents": ["Animal"],
+                "data": {
+                    "properties": {
+                        "breed": {"valueDomain": "String", "confidenceHalfDecayTime": [1, "y"]},
+                        "_specializations": {
+                            "age": {"confidenceHalfDecayTime": [1, "d"], "hooks": "inheritFrom:"},
+                            "_forThis": {"breed": {"confidenceHalfDecayTime": "inheritFrom:"}},
+                        },
+                    },
+                    "functions": {
+                        "f1": {},
+                        "f2": {"Add<Number>": {"arg1": 2, "arg2": 2}},
+                        "f3": {
+                            "interface": {"arg": "Number", "res": "Number"},
+                            "procedure": {"Return<Number>": {"what": {"Add<Number>": {"arg1": "arg", "arg2": 2}}}},
+                        },
+                        "f4": {
+                            "valueDomain": "CustomFunction",
+                            "static": True,
+                            "default": {
+                                "interface": {"arg": "Number", "res": "Number"},
+                                "procedure": {"Return<Number>": {"what": {"Add<Number>": {"arg1": "arg", "arg2": 2}}}},
+                            },
+                        },
+                        "_specializations": {
+                            "_forThis": {"f1": {"Return<Number>": {"Add<Number>": {"arg1": 2, "arg2": 2}}}}
+                        },
+                    },
+                },
+            },
+            "DogClone": "Dog",
+            "ValueDomain": {"directParents": ["Concept"], "data": {"abstract": True}},
+            "Integer": {"directParents": ["ValueDomain"], "data": {"instantiation": "integer"}},
+            "String": {
+                "directParents": ["ValueDomain"],
+                "data": {"defaultSerialization": "string", "instantiation": "string"},
+            },
+            "TypeName": {"directParents": ["String"], "data": {"instantiation": "string"}},
+            "ClosedInterval": {
+                "directParents": ["ValueDomain"],
+                "data": {
+                    "templateArguments": {"order": ["T"], "T": "Integer"},
+                    "instantiation": {
+                        "type": "object",
+                        "properties": {"min": {"type": "T"}, "max": {"type": "T"}},
+                        "additionalProperties": False,
+                    },
+                },
+            },
+            "CustomFunction": {
+                "directParents": ["ValueDomain"],
+                "data": {
+                    "instantiation": {
+                        "type": "object",
+                        "properties": {
+                            "interface": {
+                                "type": "object",
+                                "properties": {"res": {"$ref": "#/$defs/argumentTypeDefinition"}},
+                                "patternProperties": {
+                                    "^[a-z][A-Za-z0-9_]*$": {"$ref": "#/$defs/argumentTypeDefinition"}
+                                },
+                            },
+                            "procedure": "FunctionComposition",
+                        },
+                        "requiredProperties": ["procedure"],
+                        "additionalProperties": False,
+                        "$defs": {
+                            "argumentTypeDefinition": {
+                                "oneOf": [
+                                    "TypeName",
+                                    {
+                                        "type": "array",
+                                        "items": ["TypeName", "string"],
+                                        "minItems": 1,
+                                        "maxItems": 2,
+                                    },
+                                ]
+                            }
+                        },
+                    }
+                },
+            },
+            "Function": {"directParents": ["ValueDomain"], "data": {"abstract": True}},
+            "Instance": {
+                "directParents": ["ValueDomain"],
+                "data": {
+                    "templateArguments": {
+                        "order": ["AcceptConcepts...", "RejectConcepts..."],
+                        "AcceptConcepts": "And(Concept, Not(ValueDomain))",
+                        "RejectConcepts": "And(Concept, Not(ValueDomain))",
+                        "variadicGroupIdentifiers": {"AcceptConcepts": "", "RejectConcepts": "!"},
+                    }
+                },
+            },
+            "FunctionReturning": {
+                "directParents": ["Function"],
+                "data": {"abstract": True, "templateArguments": ["T"]},
+            },
+            "Add": {
+                "directParents": ["FunctionReturning"],
+                "data": {
+                    "templateArguments": {"order": ["T"], "substitutions": {"T": "T"}},
+                    "interface": {"arg1": "T", "arg2": "T", "res": "T"},
+                },
+            },
+        },
+        "instances": {
+            "MyAnimal": {"InstanceBase": {"instanceName": "MyAnimal", "concepts": ["Animal"], "properties": {"age": 2}}}
+        },
+    }
+
+    def clone_model(self) -> dict:
+        return deepcopy(self._model_data)
+
+    def get_external_data(self, x, y):
+        pass
+
+    def get_model(self, model_data) -> ConceptHierarchy:
+        model = ConceptHierarchyModel.create_from_data(model_data)
+        checker = ConceptHierarchyChecker(model, self.get_external_data)
+        res = checker.model
+        checker.check()
+        return res
+
+    def test_parsing_succeeds(self):
+        model = self.get_model(self._model_data)
+        for c_name, c in model.ch.concepts.items():
+            assert isinstance(c, HiddenImplementationDefinition) == model.ch.is_value_domain(c_name)
+            if isinstance(c, HiddenImplementationDefinition):
+                if c.is_templatable():
+                    assert c_name in ["ClosedInterval", "Instance", "FunctionReturning", "Add"]
+
+    def test_parsing_fails(self):
+        model_data = self.clone_model()
+        model_data["concepts"]["Add"]["directParents"] = ["Function"]
+        with pytest.raises(
+            CHSemanticError, match=r"Extra key\(s\) \"T\" in template substitution definition of Add must be removed!"
+        ):
+            self.get_model(model_data)
+
+    def test_parsing_succeeds_after(self):
+        self.get_model(self._model_data)
+
+    def test_specify_variadic_argument_in_constraints(self):
+        model_data = self.clone_model()
+        instance_data = model_data["concepts"]["Instance"]["data"]["templateArguments"]
+        res = instance_data.pop("AcceptConcepts")
+        instance_data["AcceptConcepts..."] = res
+        with pytest.raises(
+            CHSemanticError,
+            match=r"Can't define a constraint for 'AcceptConcepts\.\.\.' which is not a template argument of Instance\."
+            r" \n\s+It only has these template arguments: \('AcceptConcepts', 'RejectConcepts'\)",
+        ):
+            self.get_model(model_data)
+
+    def test_specify_variadic_argument_in_variadic_group_identifiers(self):
+        model_data = self.clone_model()
+        instance_data = model_data["concepts"]["Instance"]["data"]["templateArguments"]["variadicGroupIdentifiers"]
+        res = instance_data.pop("AcceptConcepts")
+        instance_data["AcceptConcepts..."] = res
+        with pytest.raises(
+            CHSyntaxError,
+            match=r"The key entry in the variadic group identifiers JSON must be a variadic template argument without "
+            r"its '...' variadic identifier!\n\s+Got 'AcceptConcepts...'",
+        ):
+            self.get_model(model_data)
---
+++ tests/unit/test_validation_of_template_instantiated_types.py
@@ -0,0 +1,176 @@
+# Copyright 2026 ConceptHierarchy Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Unit tests for checking whether the validation of template-instantiated types functions correctly.
+"""
+
+from copy import deepcopy
+
+import pytest
+
+from concept_hierarchy.data.concept_hierarchy import ConceptHierarchy
+from concept_hierarchy.errors import CHSemanticError
+from concept_hierarchy.models import ConceptHierarchyModel
+from concept_hierarchy.validator.checker import ConceptHierarchyChecker
+
+
+class TestTemplateArgumentParsing:
+    _model_data = {
+        "Concept": {},
+        "DomainConcept": {"directParents": ["Concept"], "data": {"properties": {"example": "NonTemplate1"}}},
+        "ValueDomain": {"directParents": ["Concept"], "data": {"abstract": True}},
+        "NonTemplateValueDomain": {"directParents": ["ValueDomain"], "data": {}},
+        "TemplateValueDomain": {"directParents": ["ValueDomain"], "data": {}},
+        "NonTemplate1": {"directParents": ["NonTemplateValueDomain"], "data": {}},
+        "SubNonTemplate1": {"directParents": ["NonTemplate1"], "data": {}},
+        "NonTemplate2": {"directParents": ["NonTemplateValueDomain"], "data": {}},
+        "Template1_DefaultConstraints": {
+            "directParents": ["TemplateValueDomain"],
+            "data": {"templateArguments": ["T"]},
+        },
+        "Template1": {
+            "directParents": ["TemplateValueDomain"],
+            "data": {
+                "templateArguments": {
+                    "order": ["T"],
+                    "T": "And(Concept, Not(ValueDomain))",
+                }
+            },
+        },
+        "Template2_DefaultConstraints": {
+            "directParents": ["TemplateValueDomain"],
+            "data": {"templateArguments": ["T1", "T2"]},
+        },
+        "Template2": {
+            "directParents": ["TemplateValueDomain"],
+            "data": {
+                "templateArguments": {
+                    "order": ["T1", "T2"],
+                    "T1": "And(Concept, Not(ValueDomain))",
+                    "T2": "NonTemplateValueDomain",
+                }
+            },
+        },
+        "Template3_DefaultConstraints": {
+            "directParents": ["TemplateValueDomain"],
+            "data": {"templateArguments": {"order": ["T"]}},
+        },
+        "Template3_NonTemplate": {
+            "directParents": ["TemplateValueDomain"],
+            "data": {
+                "templateArguments": {
+                    "order": ["T"],
+                    "T": "NonTemplateValueDomain",
+                }
+            },
+        },
+        "Template3_Template": {
+            "directParents": ["TemplateValueDomain"],
+            "data": {
+                "templateArguments": {
+                    "order": ["T"],
+                    "T": "TemplateValueDomain",
+                }
+            },
+        },
+        "SubTemplate1_Templated": {
+            "directParents": ["Template1"],
+            "data": {
+                "templateArguments": {"order": ["T1", "T2"], "substitutions": {"Template1:T": "T1"}},
+            },
+        },
+    }
+
+    def clone_model(self):
+        return deepcopy(self._model_data)
+
+    def shallow_copy_for_adding_or_removing_concepts(self):
+        return self._model_data.copy()
+
+    def get_external_data(self, x, y):
+        pass
+
+    def get_model(self, model_data) -> ConceptHierarchy:
+        model = ConceptHierarchyModel.create_from_data(model_data)
+        checker = ConceptHierarchyChecker(model, self.get_external_data)
+        res = checker.model
+        checker.check()
+        return res
+
+    def test_model_succeeds(self):
+        self.get_model(self._model_data)
+
+    def test_sub_templated_concept_without_substitution_fails(self):
+        new_model = self.shallow_copy_for_adding_or_removing_concepts()
+        new_model["SubTemplatedConceptWithoutSubstitution"] = {"directParents": ["Template1"], "data": {}}
+        with pytest.raises(
+            CHSemanticError,
+            match=r'Missing "templateArguments" definition in SubTemplatedConceptWithoutSubstitution, because it '
+            r'must define a substitution for "Template1:T"!',
+        ):
+            self.get_model(new_model)
+
+    def test_sub_templated_template_concept_without_substitution_fails(self):
+        new_model = self.shallow_copy_for_adding_or_removing_concepts()
+        new_model["SubTemplatedTemplateConceptWithoutSubstitution"] = {
+            "directParents": ["Template1"],
+            "data": {
+                "templateArguments": ["T1"],
+            },
+        }
+        with pytest.raises(
+            CHSemanticError,
+            match=r'Missing "substitutions" definition in SubTemplatedTemplateConceptWithoutSubstitution, '
+            r'because it must define a substitution for "Template1:T"!',
+        ):
+            self.get_model(new_model)
+
+    def test_sub_templated_template_concept_with_ambiguous_shorthand_substitution_fails(self):
+        new_model = self.shallow_copy_for_adding_or_removing_concepts()
+        new_model["SubTemplatedTemplateConceptWithAmbiguousShorthandSubstitution"] = {
+            "directParents": ["Template1", "Template3_Template"],
+            "data": {
+                "templateArguments": {"order": ["T"], "substitutions": {"T": "T"}},
+            },
+        }
+        with pytest.raises(
+            CHSemanticError,
+            match=r'\["concepts": "SubTemplatedTemplateConceptWithAmbiguousShorthandSubstitution": "data": '
+            r'"templateArguments": "substitutions" \(key\)\] \n    The substitution specification of template '
+            r"argument T is ambiguous in SubTemplatedTemplateConceptWithAmbiguousShorthandSubstitution because "
+            r"the parent concepts \[\'Template1\', \'Template3_Template\'\] define the template argument with the"
+            r' same name. Use the "<ParentConceptName>:<ParentTemplateArgumentName>" syntax to define the '
+            r"unambiguous substitution value for all parent template arguments",
+        ):
+            self.get_model(new_model)
+
+    def test_sub_templated_concept_with_ambiguous_shorthand_substitution_fails(self):
+        new_model = self.shallow_copy_for_adding_or_removing_concepts()
+        new_model["SubTemplatedConceptWithAmbiguousShorthandSubstitution"] = {
+            "directParents": ["Template1", "Template3_NonTemplate"],
+            "data": {
+                "templateArguments": {"substitutions": {"T": "NonTemplate1"}},
+            },
+        }
+        with pytest.raises(
+            CHSemanticError,
+            match=r'\["concepts": "SubTemplatedConceptWithAmbiguousShorthandSubstitution": "data": "templateArguments":'
+            r' "substitutions" \(key\)\] \n    The substitution specification of template argument T is ambiguous'
+            r" in SubTemplatedConceptWithAmbiguousShorthandSubstitution because the parent concepts "
+            r"\[\'Template1\', \'Template3_NonTemplate\'\] define the template argument with the same name. Use "
+            r'the "<ParentConceptName>:<ParentTemplateArgumentName>" syntax to define the unambiguous '
+            r"substitution value for all parent template arguments",
+        ):
+            self.get_model(new_model)
